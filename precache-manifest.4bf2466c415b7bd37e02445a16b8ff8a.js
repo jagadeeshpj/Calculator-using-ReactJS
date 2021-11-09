a3eb314d5d2aaa921687d2fada81c4940e06d7f0
JAGADEESH P <jagadeeshpj2001@gmail.com>
2021-11-09 08:57:05 +0530

    Updates

---
+++ precache-manifest.4bf2466c415b7bd37e02445a16b8ff8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44c18563299658c6e77fda73466167ec",
+    "url": "/Calculator-using-ReactJS/index.html"
+  },
+  {
+    "revision": "16f7d6541375e1e025c0",
+    "url": "/Calculator-using-ReactJS/static/css/main.d9a02207.chunk.css"
+  },
+  {
+    "revision": "b011ce21a6da794d9886",
+    "url": "/Calculator-using-ReactJS/static/js/2.add7bac3.chunk.js"
+  },
+  {
+    "revision": "16f7d6541375e1e025c0",
+    "url": "/Calculator-using-ReactJS/static/js/main.484ee1de.chunk.js"
+  },
+  {
+    "revision": "7ca014e22783c6e0d278",
+    "url": "/Calculator-using-ReactJS/static/js/runtime~main.5265f029.js"
+  }
+]);
